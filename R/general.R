057ecab4bff4f9c0feae6c1b965b81ef362cafdb
Joseph Burling <josephburling@gmail.com>
2014-12-18 19:11:05 -0600

    documentation for functions: logmean normalize snap_range

--- R/general.R
+++ R/general.R
@@ -156,3 +156,100 @@ rmPkg <- function(pkgs) {
         } else warning(simpleWarning(paste("Cannot find package with name", paste0("package:", p), "\nMake sure it has been loaded.\n")))  
     }
 }
+
+#' Snap a value to either the min or max if outside some range
+#' 
+#' If a value lies outside of some range, then this will snap to the limits
+#'
+#' Applies to vectors too
+#' 
+#' @param x numeric or integer value or vector of values
+#' @param l lower limit
+#' @param u upper limit
+#' @examples
+#' # snaps the vector below to the limits set
+#' x <- c(-2,0,0.5,1, 1.25)
+#' snap_range(x, 0, 1)
+#' @export
+snap_range <- function(x, l, u) pmax(pmin(u, x), l)
+
+#' Normalize a vector of values
+#' 
+#' Normalize a vector of values
+#'
+#' Will normalize to sum to one or take the max and use that as one.
+#' 
+#' @param x numeric or integer value or vector of values of these types
+#' @param sum2one if TRUE (default) then the vector sums to 1, else each value is a proportion of the max distance.
+#' @examples
+#' x <- runif(10, -100, 100)
+#' normalize(x, sum2one=TRUE)
+#' normalize(x, sum2one=FALSE)
+#' @export
+normalize <- function(x, sum2one=TRUE) {
+    i <- abs(min(x)) + x
+    if (sum2one) {
+        y <- i / sum(i)
+    } else {
+        y <- i / max(i)
+    }
+    return(y)
+}
+
+#' Convert milliseconds to frame number
+#' 
+#' Provide the frame rate to use to create break points from millisecond time data
+#' 
+#' Assumes time starts at 0, but this can be changed.
+#'
+#' @param x millisecond numeric or integer data
+#' @param fps frames per second. Single value.
+#' @param tstart time to be used as the initial level in a factor. Assumes 0 time.
+#' @param mscut return the cutoff points in milliseconds instead of frame number. 
+#' Similar to the millisecond vector that was entered but now binned to a specific value.
+#' @examples
+#' # sequence of milliseconds
+#' x <- seq(1, 1009, 12)
+#' 
+#' # 30 fps video
+#' ms2frames(x, fps=30)
+#' 
+#' # first frames are zero until start frame is encountered
+#' ms2frames(x, fps=29.97, tstart=333)
+#' 
+#' 
+#' names(x) <- sprintf("%.2f", ms2frames(x, fps=30, mscut=TRUE))
+#' @export
+ms2frames <- function(x, fps=30, tstart=0, mscut=FALSE) {
+    foa <- 1000 / fps
+    tend <-  max(x)
+    tinterval <- seq(tstart, tend + foa - ((tend-tstart) %% foa), foa)
+    f <- findInterval(x, tinterval, rightmost.closed=FALSE, all.inside=FALSE)
+    
+    if (any(is.na(f))) warning(simpleWarning("Found NAs for ms2frames"))
+    
+    if (mscut) {
+        return(tinterval[f])
+    } else {
+        return(f)  
+    }
+}
+
+#' Log Mean
+#' 
+#' Finds the mean by first finding log or exp then back to original scale
+#' 
+#' @param x numeric or integer value or vector of values of these types
+#' @param sum2one if TRUE (default) then the vector sums to 1, else each value is a proportion of the max distance.
+#' @examples
+#' x <- runif(10, -100, 100)
+#' normalize(x, sum2one=TRUE)
+#' normalize(x, sum2one=FALSE)
+#' @export
+logmean <- function(x, logValues=FALSE) {
+    if (logValues) {
+        return(log(mean(exp(x), na.rm=TRUE)))
+    } else {
+        return(exp(mean(log(x), na.rm=TRUE)))
+    }
+} 
