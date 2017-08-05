69e57c75657f766bc7afe9ca3633a8564d1bfc70
Joseph M. Burling <josephburling@gmail.com>
2017-08-05 12:08:25 -0700

    update package version

--- R/mejr.R
+++ R/mejr.R
@@ -1,18 +1,18 @@
 # Package info ------------------------------------------------------------
 
 #' mejr: Package
-#' 
+#'
 #' A package to distribute a set of functions and perform common operations that I've found to be
-#' useful and time saving.
-#' 
+#' useful or time saving.
+#'
 #' @section Info:
 #' See \code{library(help="mejr")} for a list of functions. \cr
 #' \cr
 #' Some useful links and external documentation \cr
 #' \cr
-#' \url{http://docs.ggplot2.org/current/}
+#' \url{http://ggplot2.tidyverse.org/reference/}
 #' \cr
-#' 
+#'
 #' @import stringr
 #' @import ggplot2
 #' @import data.table
