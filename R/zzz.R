fad597625ff2238ecece9d887a5cb4e6f1a8d460
Joseph M. Burling <josephburling@gmail.com>
2017-09-20 16:33:22 -0700

    update r options

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,19 @@
 
 # requireNamespace()
 .onLoad <- function(libname, pkgname) {
-    op <- options()
-    op.mejr <- list()
-    toset <- !(names(op.mejr) %in% names(op))
-    if(any(toset)) options(op.mejr[toset])
-    return(invisible())
+  op <- options()
+  op.mejr <- list(
+    mejr.split.tables = 110,
+    mejr.digits = 6,
+    mejr.round = 2,
+    mejr.width = 88,
+    mejr.print = 500)
+  toset <- !(names(op.mejr) %in% names(op))
+  if (any(toset)) options(op.mejr[toset])
+  return(invisible())
 }
 
 # library()
 .onAttach <- function(libname, pkgname) {
-    ggplot2::theme_set(theme_mejr())
+  ggplot2::theme_set(theme_mejr())
 }
