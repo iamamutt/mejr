1e260df63456ab7257790a14247aec64b59dfaff
Joseph M. Burling <josephburling@gmail.com>
2018-10-16 11:06:11 -0400

    updates

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@
     ggdistribute.font="Anton", mejr.pycustom="", mejr.viewdata.nrows=1000L,
     mejr.viewdata.pagesize=25L, mejr.viewdata.height="800px",
     mejr.viewdata.subsetfun="head", mejr.rfmt.cols=88, mejr.rfmt.compact=FALSE,
-    mejr.use.rfmt=FALSE, mejr.use.styler=TRUE,
+    mejr.use.rfmt=TRUE, mejr.use.styler=TRUE,
     mejr.selection.global=".last_text_selection", datatable.print.topn=25,
     datatable.print.class=TRUE, datatable.print.nrows=500, datatable.print.keys=TRUE
   )
